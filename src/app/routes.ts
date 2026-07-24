c1f222a004185a4a14bb5076264b0ac23ba8afd1
Fenil Muneer <fenilmuneer@gmail.com>
2026-07-24 23:29:28 +0530

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,11 +4,13 @@ import Home from "./pages/Home";
 import RequestAccess from "./pages/RequestAccess";
 import UploadPage from "./pages/Upload";
 import Admin from "./pages/Admin";
+import IssueDetail from "./pages/IssueDetail";
 
 export const router = createBrowserRouter([
   { path: "/login", Component: Login },
   { path: "/signup", Component: RequestAccess },
   { path: "/upload", Component: UploadPage },
   { path: "/admin", Component: Admin },
+  { path: "/issues/:id", Component: IssueDetail },
   { path: "/", Component: Home },
 ]);
